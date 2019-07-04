75414df81d694f4a691b438e25ff198bbb1529aa
Cologler <skyoflw@gmail.com>
2019-07-04 14:19:12 +0800

    support server check

---
+++ AnyPackageManagerCache/Controllers/APMC/ApmcController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnyPackageManagerCache.Controllers.APMC
+{
+    [Route("apmc/")]
+    [ApiController]
+    public class ApmcController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return this.Ok(); // mean this is a apmc server
+        }
+    }
+}
